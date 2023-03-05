1108f302061cecf9e430896a6d7720195d197428
Gennnady228 <126951061+Gennnady228@users.noreply.github.com>
2023-03-05 12:10:14 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,21 +4,9 @@ using namespace std;
 
 int main()
 {
-    int x=10,y=15;
-    float b=15.0;
-    double f=100.0;
-    bool a=true;
-    cout << "Результат умножения \n";
-    cout << x*y;
+    int x=123, y=23;
+    cout << "Резлуьтат обычного деления \n" << x / y;
     cout << "\n";
-    cout << "Результат деления без остатка \n";
-    cout << f / x;
-    cout << "\n";
-    cout << "Результат получения остатка \n";
-    cout << y % x;
-    cout << "\n";
-    cout << "Результат сложения \n";
-    cout << x + y;
-
+    cout << "Результат получения остатка \n" << x % y;
     return 0;
 }
