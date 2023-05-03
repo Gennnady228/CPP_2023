bcb3d403fe0fd913b9744fe62df7fb1c0ee93025
Gennnady228 <126951061+Gennnady228@users.noreply.github.com>
2023-05-03 08:15:51 +0800

    Rename main.cpp to S2.cpp

---
+++ S2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x=123, y=23;
+    cout << "Ðåçëóüòàò îáû÷íîãî äåëåíèÿ \n" << x / y;
+    cout << "\n";
+    cout << "Ðåçóëüòàò ïîëó÷åíèÿ îñòàòêà \n" << x % y;
+    return 0;
+}
