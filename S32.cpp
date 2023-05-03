c6d1220c9b998fe019e56d90485d217d942557d1
Gennnady228 <126951061+Gennnady228@users.noreply.github.com>
2023-05-03 08:28:16 +0800

    Add files via upload

---
+++ S32.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main() {
+    char str[100];
+    cout << "Enter a string: ";
+    cin.getline(str, 100);
+    int len = strlen(str);
+    char* ptr = str;
+    for (int i = 0; i < len; i++) {
+        if (*ptr >= 'a' && *ptr < 'z') {
+            *ptr = *ptr + 1;
+        }
+        else if (*ptr == 'z') {
+            *ptr = 'a';
+        }
+        else if (*ptr >= 'A' && *ptr < 'Z') {
+            *ptr = *ptr + 1;
+        }
+        else if (*ptr == 'Z') {
+            *ptr = 'A';
+        }
+        ptr++;
+    }
+    cout << "New string: " << str;
+    return 0;
+}
+
+
+
+
