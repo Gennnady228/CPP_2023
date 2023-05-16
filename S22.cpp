e406cd3e438e9d8ac13959027273e176d51d06d1
Gennnady228 <126951061+Gennnady228@users.noreply.github.com>
2023-05-16 20:07:48 +0800

    Update S22.cpp

--- S22.cpp
+++ S22.cpp
@@ -1,18 +1,14 @@
 #include <iostream>
+#include <fstream>
+
 using namespace std;
 
-int main() {
-    int num;
-    string numbers[10] = {"zero", "one", "two", "three", "four","five", "six", "seven", "eight", "nine"};
-
-    cout << "Vvedite chislo: ";
-    cin >> num;
-
-    if (num >= 0 && num <= 9){
-        cout << "Nazvanie chisla: " << numbers[num];
+int main(){
+    int n=1, out=0;
+    ifstream reading("output.txt");
+    while (reading >> out){
+        cout << n++ << ")" << out << '\n';
     }
-    else{
-        cout << "Wrong number!";
-    }
+    reading.close();
     return 0;
 }
