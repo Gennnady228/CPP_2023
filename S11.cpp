fd8c9230600274e3ff5a65962e2623810ef3450e
Gennnady228 <126951061+Gennnady228@users.noreply.github.com>
2023-05-31 09:51:31 +0800

    Update S11.cpp

--- S11.cpp
+++ S11.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
-
+ 
 using namespace std;
-
+ 
 int main()
 {
-    cout << "Hello world!" << endl;
+    for(int i = 0; i <= 100; i++)
+        if(i%3 == 0)
+            cout << i << endl;
     return 0;
 }
